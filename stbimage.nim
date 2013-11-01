ebbfcf7b0911e1fd6783fc07fad5a71e489e3d4f
Alasdair <alnessy@hotmail.com>
2013-11-01 16:08:12 +0100

    some code cleanup

--- stbimage.nim
+++ stbimage.nim
@@ -2,17 +2,20 @@ import unsigned
 
 # FFI procs
 
+const
+  Lib = "libstb_image.so"
+
 type
-  PUChar* = ptr cchar
+  PUChar = ptr cchar
 
 proc stbi_load(filename: cstring, x, y, comp: var cint, req_comp: int): PUChar
-  {.cdecl, importc: "stbi_load", dynlib: "libstb_image.so".}
+  {.cdecl, importc: "stbi_load", dynlib: Lib.}
 
 proc stbi_image_free(data: PUChar)
-  {.cdecl, importc: "stbi_image_free", dynlib: "libstb_image.so".}
+  {.cdecl, importc: "stbi_image_free", dynlib: Lib.}
 
 proc failure_reason*(): cstring
-  {.cdecl, importc: "stbi_failure_reason", dynlib: "libstb_image.so".}
+  {.cdecl, importc: "stbi_failure_reason", dynlib: Lib.}
 
 # Higher level wrappers
 
